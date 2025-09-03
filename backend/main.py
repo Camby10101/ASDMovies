418b75043ba8798eb6e45cf457c993888a5e5bb8
Sam Logan <logansam2021@gmail.com>
2025-09-03 19:25:59 +1000

    added supabase auth

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,34 @@
+from friends_list import FriendsList
 import uvicorn
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
+from routes.user_routes import router as user_router
+
+load_dotenv()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["X-Conversation-Id"],
+)
+
+app.include_router(user_router)
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    print("Starting Uvicorn server on http://0.0.0.0:8000")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 #Friends List System (Temp needs modifing to work with user system and database):
 
@@ -18,7 +39,7 @@ def get_friends():
     return {"owner": friends_list.owner, "friends": friends_list.get_friends()}
 
 @app.post("/friends")
-def add_friend(friend: Friend):
+def add_friend(friend):
     try:
         friends_list.add_friend(friend.name)
         return {"message": f"{friend.name} added successfully!"}
