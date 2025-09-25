400247c6560be7173b7c4c919f7d9304fb7c6ec2
dissington1 <dissington1@gmail.com>
2025-09-25 18:09:28 +1000

    Merged profile page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"; // <-- missing
 import { Typography } from "@/components/ui/typography";
 import { InfoBox } from "@/components/ui/info-box";
 import { Button } from "@/components/ui/button";
