3b2be56c7cbcb8dd56b34a4c5095ba9eaa2d0263
dissington1 <dissington1@gmail.com>
2025-09-30 22:07:59 +1000

    Removed heart icon from non-user profile view

--- frontend/src/components/ui/movieList.tsx
+++ frontend/src/components/ui/movieList.tsx
@@ -44,12 +44,14 @@ export default function MovieList({ movies }: MovieListProps) {
       {movieList.map((movie, index) => (
         <div key={movie.id} className="relative">
           <div>
-            <div className="w-1/6 absolute left-1.5 top-7.5 flex items-center justify-center bg-white rounded-md px-1 py-1 z-10">
-              <HeartRating
-                value={true}
-                onChange={() => handleRemoveFromFavourites(movie.id, index)}
-              />
-            </div>
+            {user && (
+              <div className="w-1/6 absolute left-1.5 top-7.5 flex items-center justify-center bg-white rounded-md px-1 py-1 z-10">
+                  <HeartRating
+                  value={true}
+                  onChange={() => handleRemoveFromFavourites(movie.id, index)}
+                  />
+              </div>
+            )}
             <SmallMovieCard 
               id={movie.id}
               title={movie.title}
