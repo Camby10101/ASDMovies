dbee16d312ad03d3afa7a9bd2796dc0802011f6c
Camby10101 <129371743+Camby10101@users.noreply.github.com>
2025-10-16 16:56:01 +1100

    yml v16

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: '../backend/static',
+    emptyOutDir: true
+  },
   test: {
     globals: true,
     environment: "jsdom",
